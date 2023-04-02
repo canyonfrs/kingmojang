c5829da9fe641c7b7ea0897dcf98e5caf14067b7
junghyeonsu <jung660317@naver.com>
2023-04-02 14:23:07 +0900

    feat: 다운로드 구현해보기

--- web/src/pages/HomePage.tsx
+++ web/src/pages/HomePage.tsx
@@ -1,7 +1,14 @@
 import { wrapperStyle } from "./HomePage.css";
 
 function HomePage() {
-  return <div className={wrapperStyle}>홈페이지</div>;
+  return (
+    <div className={wrapperStyle}>
+      홈페이지
+      <a download="kingmojang.dmg" href="/kingmojang-app_0.0.0-dev_aarch64.dmg">
+        kingmojang MAC 다운로드
+      </a>
+    </div>
+  );
 }
 
 export default HomePage;
