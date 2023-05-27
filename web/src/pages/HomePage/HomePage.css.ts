a5e2561e68da8aacd4fcd945d75cf87883809b3e
qkrdmstlr3 <qkrdmstlr3@naver.com>
2023-05-28 01:13:31 +0900

    feat(web): homepage에 구독중인 리스트 구현

--- web/src/pages/HomePage/HomePage.css.ts
+++ web/src/pages/HomePage/HomePage.css.ts
@@ -1,13 +1,8 @@
 import { style } from "@vanilla-extract/css";
 
 export const container = style({
-  display: "flex",
-  flexDirection: "column",
-  alignItems: "center",
-  justifyContent: "center",
-
   width: "100vw",
   height: "calc(100vh - 80px - 100px)",
 
-  backgroundColor: "#F1F1F1",
+  backgroundColor: "#F9F9F9",
 });
