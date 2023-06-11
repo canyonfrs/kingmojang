318032051b8792c62b9eda444fae6758fbeaf489
junghyeonsu <jung660317@naver.com>
2023-06-11 16:09:39 +0900

    chore(web): apis 폴더 제거 -> api 패키지로 이동

--- web/src/apis/useValidateEmail.ts
+++
@@ -1,9 +0,0 @@
-import { get } from "@kingmojang/utils";
-import { useQuery } from "@tanstack/react-query";
-
-export function useValidateEmail(email: string) {
-  const query = useQuery([email], () => {
-    return get(`members/email?email=${email}`);
-  });
-  return query;
-}
--- web/src/apis/useValidateId.ts
+++
@@ -1,9 +0,0 @@
-import { get } from "@kingmojang/utils";
-import { useQuery } from "@tanstack/react-query";
-
-export function useValidateId(name: string) {
-  const query = useQuery([name], () => {
-    return get(`members/username?username=${name}`);
-  });
-  return query;
-}
