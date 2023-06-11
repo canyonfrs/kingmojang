ae81493a039a7db4b4c07b74bb1db960c7627f9a
junghyeonsu <jung660317@naver.com>
2023-06-11 16:24:27 +0900

    fix(api): 빈값으로 들어가있는 server URL 변경

--- api/src/queries/useValidateNickname.ts
+++ api/src/queries/useValidateNickname.ts
@@ -2,11 +2,13 @@ import type { IValidateNickname } from "@kingmojang/types";
 import { useQuery } from "@tanstack/react-query";
 
 import { get } from "../axios";
+import { useBaseURL } from "../provider";
 
 // TODO(@ALL): Response type 추가해야함
 export function useValidateNickname({ nickname, type }: IValidateNickname) {
+  const baseURL = useBaseURL();
   const query = useQuery([nickname], () => {
-    return get(`members/nickname?nickname=${nickname}&type=${type}`, "");
+    return get(`members/nickname?nickname=${nickname}&type=${type}`, baseURL);
   });
   return query;
 }
