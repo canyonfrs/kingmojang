68e746ce5f42bd675b766dccabb3f99808d73cf3
junghyeonsu <jung660317@naver.com>
2023-06-11 16:10:55 +0900

    chore(web): APIProvider 적용 및 server url env 적용

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -3,21 +3,25 @@ import "@kingmojang/ui/dist/index.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { APIProvider } from "@kingmojang/api";
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import ModalProvider from "./components/contexts/ModalProvider";
+import ModalProvider from "./contexts/ModalProvider";
 import { RouterProvider } from "./Router";
 
-const queryClient = new QueryClient();
+// INFO(@정현수): https://ko.vitejs.dev/guide/env-and-mode.html
+const SERVER_URL =
+  import.meta.env.MODE === "development"
+    ? import.meta.env.VITE_SERVER_BASE_URL_DEV
+    : import.meta.env.VITE_SERVER_BASE_URL_PROD;
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
+    <APIProvider baseURL={SERVER_URL}>
       <ModalProvider>
         <RouterProvider />
       </ModalProvider>
-    </QueryClientProvider>
+    </APIProvider>
   </React.StrictMode>,
 );
