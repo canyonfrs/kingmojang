18c8b789981256e91ceb1e1c38bfa3e8e03044be
junghyeonsu <jung660317@naver.com>
2023-07-08 15:45:22 +0900

    feat(ui:Button): touchEffect 옵션 추가, 타입 변경
    - 터치 효과 사용할 지 말 지 결정 가능
    - HTMLAttributes -> ButtonHTMLAttributes (React)

--- ui/src/Button/index.tsx
+++ ui/src/Button/index.tsx
@@ -1,19 +1,29 @@
 import { mergeProps } from "@react-aria/utils";
-import type { HTMLAttributes } from "react";
+import type { ButtonHTMLAttributes } from "react";
 
 import { useTouchEffect } from "./hooks/useTouchEffect";
 import { ButtonStyle } from "./style.css";
 
-interface Props extends HTMLAttributes<HTMLButtonElement> {}
+interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
+  /**
+   * 터치 효과를 사용할지 여부를 결정합니다.
+   */
+  touchEffect?: boolean;
+}
 
 export function Button({ className, ...props }: Props) {
+  const { touchEffect, ...restProps } = props;
   const { ref, touchEffectProps } = useTouchEffect();
+
+  const buttonProps = touchEffect
+    ? mergeProps(restProps, touchEffectProps)
+    : mergeProps(restProps);
 
   return (
     <button
       ref={ref}
       className={`${ButtonStyle} ${className}`}
-      {...mergeProps(props, touchEffectProps)}
+      {...buttonProps}
     />
   );
 }
