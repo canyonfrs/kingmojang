2445e951f0a39775a046d067b3c3a30ce79654aa
junghyeonsu <jung660317@naver.com>
2023-07-16 21:21:26 +0900

    feat(app): kingmojang/api 연동

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,12 +1,21 @@
 import "./styles/global.css";
 
+import { APIProvider } from "@kingmojang/api";
 import React from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./App";
 
+// INFO(@정현수): https://ko.vitejs.dev/guide/env-and-mode.html
+const SERVER_URL =
+  import.meta.env.MODE === "development"
+    ? import.meta.env.VITE_SERVER_BASE_URL_DEV
+    : import.meta.env.VITE_SERVER_BASE_URL_PROD;
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <APIProvider baseURL={SERVER_URL}>
+      <App />
+    </APIProvider>
   </React.StrictMode>,
 );
