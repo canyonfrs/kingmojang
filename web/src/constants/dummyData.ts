58b3c58a56640761afd0eda64eda81b852a9e82e
shellboy <qkrdmstlr3@naver.com>
2023-07-30 14:38:25 +0900

    feat(web): creater페이지 memo탭 구현 (#38)
    Co-authored-by: shell <shell@toss.im>

---
+++ web/src/constants/dummyData.ts
@@ -0,0 +1,61 @@
+// FIXME(@박은식): 서버 api연결 후 지워야함
+export const SUMMARY_CARD = {
+  createrName: "풍월량",
+  createdDate: new Date("2023-04-17"),
+  modifiedDate: new Date("2023-04-17 13:02:34"),
+  profileImageSrc:
+    "https://static-cdn.jtvnw.net/jtv_user_pictures/7d354ef2-b2a8-4276-8c12-5be7f6301ae0-profile_image-300x300.png",
+  like: 2111,
+  comment: 555,
+  memo: `침착맨 = 임포스터면 말이 많아지고 신남
+  시민이면 확실히 목소리 톤 다운
+ 시민이면 활동력 저하
+
+풍월량 = 개못함 고집 셈 시야 적음
+
+김진배 = 쓰레기 살인마 잠수인지도 알면서 죽임
+  수단 방법 안가림
+
+주펄    = 아직 게임 잘 모름 안보이는 사람 찍음`,
+} as const;
+
+export const DUMMY_LIST = [
+  {
+    id: 0,
+    createrName: "풍월량",
+    createrImageUrl: "https://avatars.githubusercontent.com/u/26402298?v=4",
+  },
+  {
+    id: 1,
+    createrName: "풍월량",
+    createrImageUrl: "https://avatars.githubusercontent.com/u/26402298?v=4",
+  },
+  {
+    id: 2,
+    createrName: "풍월량",
+    createrImageUrl: "https://avatars.githubusercontent.com/u/26402298?v=4",
+  },
+  {
+    id: 3,
+    createrName: "풍월량",
+    createrImageUrl: "https://avatars.githubusercontent.com/u/26402298?v=4",
+  },
+  {
+    id: 4,
+    createrName: "풍월량",
+    createrImageUrl: "https://avatars.githubusercontent.com/u/26402298?v=4",
+  },
+  {
+    id: 5,
+    createrName: "풍월량",
+    createrImageUrl: "https://avatars.githubusercontent.com/u/26402298?v=4",
+  },
+];
+
+export const SUMMARY_CARD_LIST = [
+  SUMMARY_CARD,
+  SUMMARY_CARD,
+  SUMMARY_CARD,
+  SUMMARY_CARD,
+  SUMMARY_CARD,
+];
