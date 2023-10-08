8340b3c1ea0b8cf7db24ab39f5e2b71dfd0f4a00
Yoo Chang Heon <chyoo@rsupport.com>
2023-10-08 15:46:04 +0900

    feat: memo type 정의

--- types/src/memo.ts
+++ types/src/memo.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 export type Memo = {
   title: string;
   content: string;
@@ -18,13 +19,38 @@ export interface GetMemoListRequest {
 export type ServerMemo = Memo & {
   id: number;
   writer: string;
+=======
+interface IMemoSummary {
+  id: number;
+  title: string;
+  writer: string;
+  content: string;
+>>>>>>> c04d715 (feat: memo type 정의)
   likeCount: number;
   commentCount: number;
   createdAt: string;
   updatedAt: string;
+<<<<<<< HEAD
   isLike: boolean;
 };
 
 export interface GetMemoListResponse {
   memos: ServerMemo[];
 }
+=======
+  fontName: "string";
+  fontStyle: "string";
+  fontSize: number;
+  isLike: boolean;
+}
+
+interface IRecentMemo {
+  status: "SUCCESS" | "ERROR";
+  data: {
+    memos: IMemoSummary[];
+    hasNext: false;
+    nextPage: number;
+  };
+}
+export type { IMemoSummary, IRecentMemo };
+>>>>>>> c04d715 (feat: memo type 정의)
